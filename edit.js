62426df2dd579ba7e6c7157d1a522327519f91f9
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-03-13 17:10:04 -0600

    body edits

--- edit.js
+++ edit.js
@@ -1,7 +1,8 @@
 function editClickBody() {
   var body = document.getElementsByClassName('idea-inner')
   for (var i = 0; i < body.length; i++) {
-    body[i].addEventListener('click', editBody)
+    body[i].contentEditable = true;
+    body[i].addEventListener('blur', editBody)
   }
 }
 
@@ -9,10 +10,31 @@ function editClickBody() {
 function editClickTitle() {
   var title = document.getElementsByClassName('idea-title')
   for (var i = 0; i < title.length; i++) {
-    title[i].addEventListener('click', editBody)
+    title[i].contentEditable = true;
+    title[i].addEventListener('blur', editBody)
   }
 }
 
-function editBody(){
-  this.contentEditable = true;
+function editBody(e){
+  e.stopImmediatePropagation();
+  var ideas = JSON.parse(localStorage.getItem('ideabox'))
+  if (e.which == 13 || e.type == 'blur') {
+    var newContent = this.innerText
+    var title = this.parentElement.children[0].innerText
+  }
+  search(title, ideas, newContent)
+}
+
+function search(nameKey, localStorage, newContent){
+  for (var i = 0; i < localStorage.length; i++) {
+    if (localStorage[i].title === nameKey) {
+      updateLocal(localStorage[i], localStorage, newContent)
+    }
+  }
+}
+
+function updateLocal(idea, newIdeas, newContent){
+  idea.idea = newContent
+  localStorage.setItem('ideabox', JSON.stringify(newIdeas))
+  clearList()
 }
